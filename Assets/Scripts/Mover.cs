d8d9a49db3ff05f5d300709b9be57a65d91b9508
Amit Melamed <amitpay1234@gmail.com>
2023-04-11 19:31:31 +0300

    Final Edits

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,6 +7,10 @@ public class Mover : MonoBehaviour
     [Tooltip("Movement vector in meters per second")]
     [SerializeField] Vector3 velocity;
 
+    float time = 0;
+    [SerializeField] bool destroyAfterTime = true;
+    [SerializeField] float timeToDestroy = 10;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +21,11 @@ public class Mover : MonoBehaviour
     void Update()
     {
         transform.position += velocity * Time.deltaTime;
+        time+=Time.deltaTime;
+        if (time > timeToDestroy && destroyAfterTime) 
+        {
+            Destroy(gameObject);
+        }
     }
     public void SetVelocity(Vector3 newVelocity)
     {
